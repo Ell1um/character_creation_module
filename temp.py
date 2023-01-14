3365cbc6294bc4e2944dd8b4380706141fdab924
Валентин Бояковский <citadel.ooo@yandex.ru>
2023-01-14 23:55:12 +0700

    add work ver. without classes

--- temp.py
+++ temp.py
@@ -1,4 +1,25 @@
-def we_crash_all(name: str) -> str:    
-    return 'Привет, ' + name + ', мы всё сломали!'
 
-print(we_crash_all(100))
+from math import sqrt
+
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа')
+
+
+def calculatesquareroot(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number):
+    """Вычисляет результат."""
+    if your_number <= 0:
+        return
+    root = calculatesquareroot(your_number)
+    print(
+        f'Мы вычислили квадратный корень из введённого вами числа.'
+        f'Это будет: {root}'
+        )
+
+
+print(message)
+calc(25.5)
